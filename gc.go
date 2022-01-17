82add6f990abb5a6d2543f52af49a2d9410657f9
peyman <soleymanip291@gmail.com>
2022-01-17 15:45:51 +0330

    test

---
+++ gc.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func printStats(mem runtime.MemStats) {
+
+	runtime.ReadMemStats(&mem)
+	fmt.Println("mem.Alloc:", mem.Alloc)
+	fmt.Println("mem.TotalAloc:", mem.TotalAlloc)
+	fmt.Println("mem.NumGC:", mem.NumGC)
+	fmt.Println("--------")
+
+}
+
+func main() {
+
+	var mem runtime.MemStats
+	printStats(mem)
+
+	for i := 0; i < 10; i++ {
+
+		s := make([]byte, 50000000)
+		if s == nil {
+			fmt.Println("Operation Failed")
+
+		}
+
+	}
+
+}
